157b649cc7b41d9bc1fe7edc54d43824d4ab32bd
SergeyLebidko <it.sergeyler@mail.ru>
2022-01-04 13:06:11 +0300

    Добавлена стилизация в зависимости от состояния переноса

--- src/components/TaskPanel/TaskPanel.tsx
+++ src/components/TaskPanel/TaskPanel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Task} from '../../types';
 import './TaskPanel.scss';
 
@@ -6,58 +6,60 @@ type TaskProps = {
     task: Task
 }
 
+enum DNDMode {
+    NoDrag,    // Нормальное сосотояние элемента
+    Dragged,   // Элемент перетаскивается пользователем
+    Dropped    // Элемент является целью перетаскивания
+}
+
 const TaskPanel: React.FC<TaskProps> = ({task}) => {
+    const [dndMode, setDndMode] = useState<DNDMode>(DNDMode.NoDrag);
+
     const {id, title} = task;
+
+    const getClasses = (): string => {
+        const classes = ['task_panel'];
+        if (dndMode === DNDMode.NoDrag) classes.push('task_panel_normal');
+        if (dndMode === DNDMode.Dragged) classes.push('task_panel_dragged');
+        if (dndMode === DNDMode.Dropped) classes.push('task_panel_dropped');
+        return classes.join(' ');
+    };
 
     // ---------- на источнике ----------
 
     const dragStartHandler = (event: React.DragEvent<HTMLLIElement>): void => {
-        event.dataTransfer.setData('Text', '' + id);
-
-        // const id = (event.target as HTMLLIElement).dataset.id;
-        // console.log('dragStart', id);
+        event.dataTransfer.setData('Text', JSON.stringify(task));
+        setDndMode(DNDMode.Dragged);
     }
 
-    const dragEndHandler = (event: React.DragEvent<HTMLLIElement>): void => {
-        // event.preventDefault();
-        // event.stopPropagation();
-        // const id = (event.target as HTMLLIElement).dataset.id;
-        // console.log('dragEnd', id);
+    const dragEndHandler = (): void => {
+        setDndMode(DNDMode.NoDrag);
     }
 
     // ---------- на приемнике ----------
 
-    const dragEnterHandler = (event: React.DragEvent<HTMLLIElement>): void => {
-        // event.preventDefault();
-        // event.stopPropagation();
-        // const id = (event.target as HTMLLIElement).dataset.id;
-        // console.log('dragEnter', id);
+    const dragEnterHandler = (): void => {
+        setDndMode(oldMode => oldMode === DNDMode.NoDrag ? DNDMode.Dropped : oldMode);
     }
 
     const dragOverHandler = (event: React.DragEvent<HTMLLIElement>): void => {
         event.preventDefault();
         event.stopPropagation();
-        // const id = (event.target as HTMLLIElement).dataset.id;
-        // console.log('dragOver', id);
     }
 
-    const dragLeaveHandler = (event: React.DragEvent<HTMLLIElement>): void => {
-        // event.preventDefault();
-        // event.stopPropagation();
-        // const id = (event.target as HTMLLIElement).dataset.id;
-        // console.log('dragLeave', id);
+    const dragLeaveHandler = (): void => {
+        setDndMode(oldMode => oldMode === DNDMode.Dropped ? DNDMode.NoDrag : oldMode);
     }
 
     const dropHandler = (event: React.DragEvent<HTMLLIElement>): void => {
         event.preventDefault();
         event.stopPropagation();
-        const id = (event.target as HTMLLIElement).dataset.id;
-        console.log('drop', event.dataTransfer.getData('Text'), id);
+        setDndMode(DNDMode.NoDrag);
     }
 
     return (
         <li
-            className="task_panel"
+            className={getClasses()}
             data-id={id}
             draggable
             onDragStart={dragStartHandler}
