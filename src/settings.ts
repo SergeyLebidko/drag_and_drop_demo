48e2dd001d5e8ba4177c07b7d00838c37810fab4
SergeyLebidko <it.sergeyler@mail.ru>
2022-01-05 11:23:02 +0300

    Скорректировано начальное количество объектов

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
-export const CARD_MIN_COUNT = 3
-export const CARD_MAX_COUNT = 6;
-export const TASK_MIN_COUNT = 5
-export const TASK_MAX_COUNT = 10
+export const CARD_MIN_COUNT = 2
+export const CARD_MAX_COUNT = 5;
+export const TASK_MIN_COUNT = 3
+export const TASK_MAX_COUNT = 8
