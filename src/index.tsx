93bf95df523bbdb2d586b77d756f6499dc8c3573
SergeyLebidko <it.sergeyler@mail.ru>
2022-01-15 15:47:23 +0300

    Создан компонент для глобального сброса стилей

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.scss';
 import App from './components/App/App';
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  h1 {
+    margin: 0;
+  }
+`
 
 ReactDOM.render(
-    <App/>,
+    <>
+        <GlobalStyle/>
+        <App/>
+    </>,
     document.getElementById('root')
 );
